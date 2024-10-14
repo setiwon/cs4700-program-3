4bb0a686fe8198f5874fb667afaa5d2b1b2d1c6d
Carlos Castrillo <carcast1900@gmail.com>
2024-10-13 19:55:56 -0700

    Added movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public float speed = 10;
+    public Rigidbody2D rb;
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(x, y, 0);
+        movement = movement.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.transform.position + movement);
+
+    }
+}
